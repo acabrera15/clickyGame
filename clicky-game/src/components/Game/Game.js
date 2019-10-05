c41b0a1870388c4d62195b089f7c55b488f4988b
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-10-04 18:13:32 -0700

    added an id to the picture arrray

--- clicky-game/src/components/Game/Game.js
+++ clicky-game/src/components/Game/Game.js
@@ -5,42 +5,84 @@ import Navbar from "../Navbar/Navbar";
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      clickedItem: []
+    };
   }
   pictureSources = [
-    "./images/anibal-renones-u9nYWmDjK7U-unsplash.jpg",
-    "./images/anthony-rosset-nMyYlVWOzzc-unsplash.jpg",
-    "./images/bmarcel-W9_RUKaK48E-unsplash.jpg",
-    "./images/clem-onojeghuo-pTeZKi29EYE-unsplash.jpg",
-    "./images/fermin-rodriguez-penelas-a1dpvJv45To-unsplash.jpg",
-    "./images/fermin-rodriguez-penelas-r3muakP0QuE-unsplash.jpg",
-    "./images/michael-We2t3suGiYk-unsplash.jpg",
-    "./images/olu-famule-D1YCMXkiSAI-unsplash.jpg",
-    "./images/stanley-shashi-Pm_926lM-xI-unsplash.jpg",
-    "./images/stephan-valentin-pwEhZkeTels-unsplash.jpg",
-    "./images/tanalee-youngblood-vIDb3hNyw9s-unsplash.jpg",
-    "./images/wayne-robinson-Ud9lKknu9c0-unsplash.jpg"
+    {
+      src: "./images/anibal-renones-u9nYWmDjK7U-unsplash.jpg",
+      id: 0
+    },
+    {
+      src: "./images/anthony-rosset-nMyYlVWOzzc-unsplash.jpg",
+      id: 1
+    },
+    {
+      src: "./images/bmarcel-W9_RUKaK48E-unsplash.jpg",
+      id: 2
+    },
+    {
+      src: "./images/clem-onojeghuo-pTeZKi29EYE-unsplash.jpg",
+      id: 3
+    },
+    {
+      src: "./images/fermin-rodriguez-penelas-a1dpvJv45To-unsplash.jpg",
+      id: 4
+    },
+    {
+      src: "./images/fermin-rodriguez-penelas-r3muakP0QuE-unsplash.jpg",
+      id: 5
+    },
+    {
+      src: "./images/michael-We2t3suGiYk-unsplash.jpg",
+      id: 6
+    },
+    {
+      src: "./images/olu-famule-D1YCMXkiSAI-unsplash.jpg",
+      id: 7
+    },
+    {
+      src: "./images/stanley-shashi-Pm_926lM-xI-unsplash.jpg",
+      id: 8
+    },
+    {
+      src: "./images/stephan-valentin-pwEhZkeTels-unsplash.jpg",
+      id: 9
+    },
+    {
+      src: "./images/tanalee-youngblood-vIDb3hNyw9s-unsplash.jpg",
+      id: 10
+    },
+    {
+      src: "./images/wayne-robinson-Ud9lKknu9c0-unsplash.jpg",
+      id: 11
+    }
   ];
 
   randomizeTile = () => {
-
     var currentIndex = this.pictureSources.length;
-	var temporaryValue, randomIndex;
+    var temporaryValue, randomIndex;
 
-	// While there remain elements to shuffle...
-	while (0 !== currentIndex) {
-		// Pick a remaining element...
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-		// And swap it with the current element.
-		temporaryValue = this.pictureSources[currentIndex];
-		this.pictureSources[currentIndex] = this.pictureSources[randomIndex];
-		this.pictureSources[randomIndex] = temporaryValue;
+      // And swap it with the current element.
+      temporaryValue = this.pictureSources[currentIndex];
+      this.pictureSources[currentIndex] = this.pictureSources[randomIndex];
+      this.pictureSources[randomIndex] = temporaryValue;
     }
-    
+
     return this.pictureSources;
+  };
+
+  handleClick(event) {
+    event.preventDefault();
   }
+
   render() {
     return (
       <div>
@@ -48,8 +90,8 @@ class Game extends React.Component {
 
         <div className="container">
           <div className="row">
-            {this.randomizeTile().map(item => {
-              return <Tile srcImage={item} />;
+            {this.randomizeTile().map((item, i) => {
+              return <Tile key={i} id={item.id} srcImage={item.src} />;
             })}
           </div>
         </div>
--- clicky-game/src/components/Tile/TIle.js
+++ clicky-game/src/components/Tile/TIle.js
@@ -3,14 +3,16 @@ import React from "react";
 class Tile extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+        clicked: false
+    };
   }
 
   render() {
-    console.log(this.props.srcImage)
+    console.log(this.props.id)
 
     return (
-    <div className="card" style={{ width: "18rem", height: '18rem'}}>
+    <div className="card" style={{ width: "18rem", height: '18rem', marginRight: '2%', marginTop: '2%'}}>
         <img style={{ width: "18rem", height: '18rem'}} src={this.props.srcImage} className="card-img-top" alt="..." />
       </div>
     );
