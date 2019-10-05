1231f0ef5e2d27ee1c92500a98688a6301aae05d
Andrew Cabrera <andrewcabrera7@gmail.com>
2019-10-04 17:09:48 -0700

    tiles are now random

--- clicky-game/src/components/Game/Game.js
+++ clicky-game/src/components/Game/Game.js
@@ -5,31 +5,42 @@ import Navbar from "../Navbar/Navbar";
 class Game extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      pictureSources: [
-        "./images/anibal-renones-u9nYWmDjK7U-unsplash.jpg",
-        "./images/anthony-rosset-nMyYlVWOzzc-unsplash.jpg",
-        "./images/bmarcel-W9_RUKaK48E-unsplash.jpg",
-        "./images/clem-onojeghuo-pTeZKi29EYE-unsplash.jpg",
-        "./images/fermin-rodriguez-penelas-a1dpvJv45To-unsplash.jpg",
-        "./images/fermin-rodriguez-penelas-r3muakP0QuE-unsplash.jpg",
-        "./images/michael-We2t3suGiYk-unsplash.jpg",
-        "./images/olu-famule-D1YCMXkiSAI-unsplash.jpg",
-        "./images/stanley-shashi-Pm_926lM-xI-unsplash.jpg",
-        "./images/stephan-valentin-pwEhZkeTels-unsplash.jpg",
-        "./images/tanalee-youngblood-vIDb3hNyw9s-unsplash.jpg",
-        "./images/wayne-robinson-Ud9lKknu9c0-unsplash.jpg"
-      ]
-    };
+    this.state = {};
   }
+  pictureSources = [
+    "./images/anibal-renones-u9nYWmDjK7U-unsplash.jpg",
+    "./images/anthony-rosset-nMyYlVWOzzc-unsplash.jpg",
+    "./images/bmarcel-W9_RUKaK48E-unsplash.jpg",
+    "./images/clem-onojeghuo-pTeZKi29EYE-unsplash.jpg",
+    "./images/fermin-rodriguez-penelas-a1dpvJv45To-unsplash.jpg",
+    "./images/fermin-rodriguez-penelas-r3muakP0QuE-unsplash.jpg",
+    "./images/michael-We2t3suGiYk-unsplash.jpg",
+    "./images/olu-famule-D1YCMXkiSAI-unsplash.jpg",
+    "./images/stanley-shashi-Pm_926lM-xI-unsplash.jpg",
+    "./images/stephan-valentin-pwEhZkeTels-unsplash.jpg",
+    "./images/tanalee-youngblood-vIDb3hNyw9s-unsplash.jpg",
+    "./images/wayne-robinson-Ud9lKknu9c0-unsplash.jpg"
+  ];
 
-  returnTiles = () => {
-    this.state.pictureSources.forEach(item => {
-      console.log(item);
-      return <Tile srcImage={item} />;
-    });
-  };
+  randomizeTile = () => {
 
+    var currentIndex = this.pictureSources.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = this.pictureSources[currentIndex];
+		this.pictureSources[currentIndex] = this.pictureSources[randomIndex];
+		this.pictureSources[randomIndex] = temporaryValue;
+    }
+    
+    return this.pictureSources;
+  }
   render() {
     return (
       <div>
@@ -37,7 +48,7 @@ class Game extends React.Component {
 
         <div className="container">
           <div className="row">
-            {this.state.pictureSources.map(item => {
+            {this.randomizeTile().map(item => {
               return <Tile srcImage={item} />;
             })}
           </div>
